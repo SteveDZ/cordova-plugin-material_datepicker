84f20d1a39f5d64dbaee95176c775a73b150161c
Steve De Zitter <de.zitter.steve@gmail.com>
2016-09-13 12:47:53 +0200

    JSON.stringify the hook context object

--- www/android/hooks/before_prepare/copy_android_theme.js
+++ www/android/hooks/before_prepare/copy_android_theme.js
@@ -12,7 +12,7 @@ module.exports = function(ctx) {
     var srcFile = path.join(themeRoot, 'styles.xml');
     var destFile = path.join(platformRoot, 'res/values/styles.xml');
 
-    console.log('Copying files...: ' + ctx);
+    console.log('Copying files...: ' + JSON.stringify(ctx));
 
     fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
 }

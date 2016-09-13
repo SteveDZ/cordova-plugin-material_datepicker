cecf09db76d691c6c2c79baa9a92c368d2f79fbd
Steve De Zitter <de.zitter.steve@gmail.com>
2016-09-13 09:35:16 +0200

    Removed check for existance of both source and destination file

--- www/android/hooks/before_prepare/copy_android_theme.js
+++ www/android/hooks/before_prepare/copy_android_theme.js
@@ -12,11 +12,7 @@ module.exports = function(ctx) {
     var srcFile = path.join(themeRoot, 'styles.xml');
     var destFile = path.join(platformRoot, 'res/values/styles.xml');
 
-    if(fs.existsSync(srcFile) && fs.existsSync(destFile)) {
-        console.log('Copying files...');
+    console.log('Copying files...');
 
-        fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
-    }else{
-        console.log('source of destination file does not exist, srcFile: ' + srcFile + ', destFile: ' + destFile);
-    }
+    fs.createReadStream(srcFile).pipe(fs.createWriteStream(destFile));
 }
